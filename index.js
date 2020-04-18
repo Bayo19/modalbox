9048c311aada05772cb8843bf3d0ac2f44fedce9
Bayo Ade <bayo.300@gmail.com>
2020-04-18 16:12:22 +0100

    trying some stuff that I'll reuse else where, maybe

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ let modalBtn = document.querySelector('.btn')
 let modalBg = document.querySelector('.modal-bg')
 let modalClose = document.querySelector('.modal-close')
 
+let header = document.querySelector('.header')
+
 modalBtn.addEventListener('click', function() {
     modalBg.classList.add('bg-active')
+    let col = getComputedStyle(modalBtn)
+    header.style.color = col.backgroundColor
 })
 
 modalClose.addEventListener('click', function() {
